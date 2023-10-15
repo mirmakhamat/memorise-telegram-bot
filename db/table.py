c32dddbd8bebb384e24a9dddf88ce5b783444274
Javohir Elmurodov <elmurodovjavohir@gmail.com>
2023-10-15 16:50:17 +0500

    fix: user table as base class

--- db/table.py
+++ db/table.py
@@ -5,16 +5,18 @@ import enum
 meta = MetaData()
 Base = declarative_base()
 
-UsersTable = Table(
-    'users', meta,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('first_name', String),
-    Column('last_name', String),
-    Column('telegram_id', Integer, unique=True),
-    Column('created_at', DateTime, default=datetime.datetime.now),
-    Column('updated_at', DateTime, default=datetime.datetime.now,
-           onupdate=datetime.datetime.now)
-)
+
+class UsersTable(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    telegram_id = Column(Integer, unique=True)
+
+    created_at = Column(DateTime, default=datetime.datetime.now)
+    updated_at = Column(DateTime, default=datetime.datetime.now,
+                        onupdate=datetime.datetime.now)
 
 
 class WordsTable(Base):
